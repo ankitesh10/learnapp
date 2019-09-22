0f0d8c643d2873121b7a84045cbec1eb336982c8
ankitesh <ankitesh4@gmail.com>
2019-09-23 03:19:54 +0530

    production build added

---
+++ build/precache-manifest.cf4b9d4647b734adef99e52ac1e5ab7f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c04e892a66f51203f3d264e9da11cb7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa5ebbb4471d3d3dbdcc",
+    "url": "/static/css/main.3ef695c1.chunk.css"
+  },
+  {
+    "revision": "1346f42f697f53d2b66e",
+    "url": "/static/js/2.1516a379.chunk.js"
+  },
+  {
+    "revision": "aa5ebbb4471d3d3dbdcc",
+    "url": "/static/js/main.7e0035a9.chunk.js"
+  },
+  {
+    "revision": "b6b96fb257384fed1f54",
+    "url": "/static/js/runtime~main.009ad60e.js"
+  }
+]);
